b871611203b433fb042dd0d9dfa02c9e96d71408
Ana Tatar <94306011+anatatar10@users.noreply.github.com>
2024-10-25 09:06:07 +0300

    lab 1

---
+++ roboti/catckin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/pi/catckin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/pi/catckin_ws/devel;/home/pi/my_robot/devel;/home/pi/puppy_pi/devel;/opt/ros/noetic'.split(';') if '/home/pi/catckin_ws/devel;/home/pi/my_robot/devel;/home/pi/puppy_pi/devel;/opt/ros/noetic' != '' else []
---
+++ roboti/catckin_ws/build/pkg_1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/catckin_ws/src/pkg_1
+# Build directory: /home/pi/catckin_ws/build/pkg_1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
